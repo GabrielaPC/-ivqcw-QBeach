6d5a6c6e389fe3a8db34e3ea8ffc8bf7d10820db
Gabriela Pinheiro <gabrielapc@id.uff.br>
2021-10-30 10:03:33 -0300

    Questao2.py

---
+++ Q2/Questao2.py
@@ -0,0 +1,178 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import BasicAer, execute
+
+from numpy import pi
+
+# NÃO ENVIAR !!!!!!!!!
+def Y(qc):
+  qc.y(0)
+
+def ZX(qc):
+  qc.z(0)
+  qc.x(0)
+
+def mY(qc):
+  qc.z(0)
+  qc.x(0)
+  qc.y(0)
+  qc.z(0)
+  qc.x(0)
+
+def mZX(qc):
+  qc.x(0)
+  qc.z(0)
+
+
+def roda_e_plota(cir, backend = "qasm_simulator"):
+  be = BasicAer.get_backend(backend)
+  job = execute(cir, be, shots=1)
+  result = job.result()
+  counts = result.get_counts(cir)
+  plot_histogram([counts])
+
+def circuito_Y():
+  q = QuantumRegister(2, 'q')
+  c = ClassicalRegister(2, 'c')
+  circuit = QuantumCircuit(q, c)
+  
+  #estado de bell
+  circuit.h(q)
+  circuit.cz(q[0],q[1])
+  
+  #inserção de U 
+  circuit.barrier(q[0], q[1])
+  Y(circuit)
+  circuit.barrier(q[0], q[1])
+  
+  
+  # inversão sobre a média 
+  circuit.h(q[0])
+  circuit.h(q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.cz(q[0], q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.h(q[0])
+  circuit.h(q[1])
+  
+  
+  
+  
+  circuit.measure(q[0], c[0])
+  circuit.measure(q[1], c[1])
+  
+  
+  roda_e_plota(circuit)
+  
+def circuito_ZX():
+  q = QuantumRegister(2, 'q')
+  c = ClassicalRegister(2, 'c')
+  circuit = QuantumCircuit(q, c)
+  
+  #estado de bell
+  circuit.h(q)
+  circuit.cz(q[0],q[1])
+  
+  #inserção de U 
+  circuit.barrier(q[0], q[1])
+  ZX(circuit)
+  circuit.barrier(q[0], q[1])
+  
+  
+  # inversão sobre a média 
+  circuit.h(q[0])
+  circuit.h(q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.cz(q[0], q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.h(q[0])
+  circuit.h(q[1])
+  
+  
+  
+  
+  circuit.measure(q[0], c[0])
+  circuit.measure(q[1], c[1])
+  
+  
+  roda_e_plota(circuit)
+
+def circuito_mY():
+  q = QuantumRegister(2, 'q')
+  c = ClassicalRegister(2, 'c')
+  circuit = QuantumCircuit(q, c)
+  
+  #estado de bell
+  circuit.h(q)
+  circuit.cz(q[0],q[1])
+  
+  #inserção de U 
+  circuit.barrier(q[0], q[1])
+  mY(circuit)
+  circuit.barrier(q[0], q[1])
+  
+  
+  # inversão sobre a média 
+  circuit.h(q[0])
+  circuit.h(q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.cz(q[0], q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.h(q[0])
+  circuit.h(q[1])
+  
+  
+  
+  
+  circuit.measure(q[0], c[0])
+  circuit.measure(q[1], c[1])
+  
+  
+  roda_e_plota(circuit)
+
+def circuito_mZX():
+  q = QuantumRegister(2, 'q')
+  c = ClassicalRegister(2, 'c')
+  circuit = QuantumCircuit(q, c)
+  
+  #estado de bell
+  circuit.h(q)
+  circuit.cz(q[0],q[1])
+  
+  #inserção de U 
+  circuit.barrier(q[0], q[1])
+  mZX(circuit)
+  circuit.barrier(q[0], q[1])
+  
+  
+  # inversão sobre a média 
+  circuit.h(q[0])
+  circuit.h(q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.cz(q[0], q[1])
+  circuit.x(q[0])
+  circuit.x(q[1])
+  circuit.h(q[0])
+  circuit.h(q[1])
+  
+  
+  
+  
+  circuit.measure(q[0], c[0])
+  circuit.measure(q[1], c[1])
+  
+  
+  roda_e_plota(circuit)
+  
+  
+
+circuito_Y()
+circuito_ZX()
+circuito_mY()
+circuito_mZX()
